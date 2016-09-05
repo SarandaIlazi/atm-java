669fe477583b775e062ce5125132f98aee82651b
SarandaIlazi <saranda.ilazi9@gmail.com>
2016-09-06 01:26:24 +0200

    First Change

--- src/com/si/Main.java
+++ src/com/si/Main.java
@@ -1,8 +1,8 @@
 package com.si;
 
-public class Main {
+public class  Main {
 
     public static void main(String[] args) {
-	// write your code here
+	System.out.println("TEST HYHY");
     }
 }
---
+++ src/com/si/domain/Client.java
@@ -0,0 +1,7 @@
+package com.si.domain;
+
+/**
+ * Created by Saranda Ilazi on 06/09/2016.
+ */
+public class Client {
+}
