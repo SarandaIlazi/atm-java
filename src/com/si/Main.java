a91fc0f4355f157ba3eb97ae18b2da048868482c
SarandaIlazi <saranda.ilazi9@gmail.com>
2016-09-14 23:41:17 +0200

    Second update

--- src/com/si/Main.java
+++ src/com/si/Main.java
@@ -1,8 +1,18 @@
 package com.si;
+import java.util.*;
 
 public class  Main {
 
     public static void main(String[] args) {
-	System.out.println("TEST HYHY");
+        System.out.print("Enter your username: ");
+        DeafultData defaultData = new DeafultData();
+        Scanner scan1 = new Scanner(System.in);
+        String inputName = scan1.nextLine();
+
+        System.out.print("Enter your surname: ");
+        Scanner scan2 = new Scanner(System.in);
+        String inputSurname = scan2.next();
+
+
     }
 }
--- src/com/si/domain/Client.java
+++ src/com/si/domain/Client.java
@@ -1,7 +1,114 @@
 package com.si.domain;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Created by Saranda Ilazi on 06/09/2016.
  */
 public class Client {
+    private int id;
+    private String name;
+    private String surname;
+    private String city;
+    private String address;
+    private Date birthday;
+    private List<Account> accounts;
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String surname, String city, String address, Date birthday, List<Account> accounts) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.city = city;
+        this.address = address;
+        this.birthday = birthday;
+        this.accounts = accounts;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(List<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", city='" + city + '\'' +
+                ", address='" + address + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Client client = (Client) o;
+
+        return id == client.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
